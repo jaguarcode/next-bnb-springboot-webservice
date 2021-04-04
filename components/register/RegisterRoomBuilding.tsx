dc785e9c588aebb0deb451be55283219282dcb68
IKHYEON KIM <behonz@gmail.com>
2021-04-04 16:33:30 +0900

    숙소 등록하기 - 1단계 (게스트 관련 옵션) 추가

--- components/register/RegisterRoomBuilding.tsx
+++ components/register/RegisterRoomBuilding.tsx
@@ -5,6 +5,8 @@ import { useSelector } from "../../store";
 import palette from "../../styles/palette";
 import Selector from "../common/selector/Selector";
 import { registerRoomActions } from "../../store/registerRoom";
+import RadioGroup from "../common/RadioGroup";
+import RegisterRoomFooter from "./RegisterRoomFooter";
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -22,6 +24,13 @@ const Container = styled.div`
     width: 320px;
     margin-bottom: 32px;
   }
+  .register-room-room-type-radio {
+    max-width: 485px;
+    margin-bottom: 50px;
+  }
+  .register-room-is-setup-for-guest-radio {
+    margin-bottom: 50px;
+  }
 `;
 
 const RegisterRoomBuilding: React.FC = () => {
@@ -30,6 +39,10 @@ const RegisterRoomBuilding: React.FC = () => {
   );
 
   const buildingType = useSelector((state) => state.registerRoom.buildingType);
+  const roomType = useSelector((state) => state.registerRoom.roomType);
+  const isSetUpForGuest = useSelector(
+    (state) => state.registerRoom.isSetUpForGuest
+  );
 
   const dispatch = useDispatch();
 
@@ -82,6 +95,14 @@ const RegisterRoomBuilding: React.FC = () => {
     }
   }, [largeBuildingType]);
 
+  //* 모든 값이 있는지 확인하기
+  const isValid = useMemo(() => {
+    if (!largeBuildingType || !buildingType || !roomType || !isSetUpForGuest) {
+      return false;
+    }
+    return true;
+  }, [largeBuildingType, buildingType, roomType, isSetUpForGuest]);
+
   return (
     <Container>
       <h2>등록할 숙소 종류는 무엇인가요?</h2>
@@ -118,6 +139,69 @@ const RegisterRoomBuilding: React.FC = () => {
           options={detailBuildingOptions}
         />
       </div>
+      {buildingType && (
+        <>
+          <div className="register-room-room-type-radio">
+            <RadioGroup
+              label="게스트가 묵게 될 숙소 유형을 골라주세요."
+              value={roomType}
+              isValid={!!roomType}
+              onChange={(selected) =>
+                dispatch(
+                  registerRoomActions.setRoomType(
+                    selected as "entire" | "private" | "public"
+                  )
+                )
+              }
+              options={[
+                {
+                  label: "집 전체",
+                  value: "entire",
+                  description:
+                    "게스트가 숙소 전체를 다른 사람과 공유하지 않고 단독으로 이용합니다. 일반적으로 침실, 욕실, 부엌이 포함됩니다.",
+                },
+                {
+                  label: "개인실",
+                  value: "private",
+                  description:
+                    "게스트에게 개인 침실이 제공됩니다. 침실 이외의 공간은 공용일 수 있습니다.",
+                },
+                {
+                  label: "다인실",
+                  value: "public",
+                  description:
+                    "게스트는 개인 공간 없이, 다른 사람과 함께 쓰는 침실이나 공용 공간에서 숙박합니다.",
+                },
+              ]}
+            />
+          </div>
+          <div className="register-room-is-setup-for-guest-radio">
+            <RadioGroup
+              label="게스트만 사용하도록 만들어진 숙소인가요?"
+              value={isSetUpForGuest}
+              isValid={isSetUpForGuest !== null}
+              onChange={(value) =>
+                dispatch(registerRoomActions.setIsSetUpForGuest(value))
+              }
+              options={[
+                {
+                  label: "예. 게스트용으로 따로 마련된 숙소입니다.",
+                  value: true,
+                },
+                {
+                  label: "아니요. 제 개인 물건이 숙소에 있습니다.",
+                  value: false,
+                },
+              ]}
+            />
+          </div>
+        </>
+      )}
+      <RegisterRoomFooter
+        isValid={isValid}
+        prevHref="/"
+        nextHref="/room/register/bedrooms"
+      />
     </Container>
   );
 };
